efb465ce6d23d299192727f059e15edaf8347cac
erdemsword <erik001.ronaldinho@gmail.com>
2021-05-03 23:11:00 +0300

    Update tahmin.cob

--- tahmin.cob
+++ tahmin.cob
@@ -1,16 +1,26 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. tahmin.
        AUTHOR. Erdem.
-       DATE-WRITTEN. 12 Nisan.
+       DATE-WRITTEN. 05 Mayis.
        ENVIRONMENT DIVISION.
 
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-       01 tahmin_sayi PIC 9 VALUE 7.
-       01 tahmin_kalan PIC 9 VALUE 3.
-       01 tahmin_sayi_input PIC 9.
+       01 tahmin_min PIC 9(1) VALUE 1.
+       01 tahmin_max PIC 9(1) VALUE 9.
+       01 tahmin_sayi PIC 9(1).
+       01 tahmin_kalan PIC 9(1) VALUE 3.
+       01 tahmin_sayi_input PIC 9(1).
        PROCEDURE DIVISION.
+
+           RANDOM-NUMBER.
+           PERFORM 9 TIMES
+               COMPUTE tahmin_sayi = FUNCTION RANDOM() *
+                       (tahmin_max - tahmin_min + 1) + tahmin_min
+               DISPLAY "RASTGELE SAYI = " tahmin_sayi
+           END-PERFORM.
+
            FIRST-CODE.
            PERFORM SECOND-CODE WITH TEST AFTER UNTIL tahmin_kalan < 1.
            STOP RUN.
