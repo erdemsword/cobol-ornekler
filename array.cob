4fff39e93c5f94fcc4ec848b6715bb0448d230cc
erdemsword <erik001.ronaldinho@gmail.com>
2021-05-07 01:58:39 +0300

    Update array.cob

--- array.cob
+++ array.cob
@@ -2,35 +2,37 @@
        PROGRAM-ID. ARRAY.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 WS-BEGIN PIC 9(2).
+       01 WS-START PIC 9(2).
        01 WS-END PIC 9(2).
        01 WS-TARGET PIC 9(2).
        01 WS-NUMBER PIC 9(2).
+       01 WS-PERFORM PIC 9(2).
        PROCEDURE DIVISION.
            DISPLAY "Baslangic sayisi gir = "
-           ACCEPT WS-BEGIN
+           ACCEPT WS-START
            DISPLAY "Bitis sayisi gir = "
            ACCEPT WS-END
            DISPLAY "Hedef sayiyi gir = "
            ACCEPT WS-TARGET
 
-           IF WS-BEGIN IS GREATER WS-END THEN
+           IF WS-START IS GREATER WS-END THEN
                DISPLAY 
                "Baslangic sayisi bitis sayisindan buyuk olmamali"
                STOP RUN
            END-IF.
-           IF WS-TARGET IS GREATER WS-END OR NOT GREATER WS-BEGIN THEN
+           IF WS-TARGET IS GREATER WS-END OR NOT GREATER WS-START THEN
                DISPLAY 
                "Hedef sayi baslangic sayi ila bitis sayi arasnda olmali"
                STOP RUN
            END-IF.
            DISPLAY "-----------------------"
-           PERFORM WS-END TIMES
-               COMPUTE WS-NUMBER = WS-NUMBER + 1
-               IF WS-NUMBER EQUAL TO WS-TARGET THEN
-                   DISPLAY WS-NUMBER " - hedef sayi"
+           COMPUTE WS-PERFORM = WS-END - WS-START
+           PERFORM WS-PERFORM TIMES
+               COMPUTE WS-START = (WS-START) + 1
+               IF WS-START EQUAL TO WS-TARGET THEN
+                   DISPLAY WS-START " - hedef sayi"
                ELSE
-                   DISPLAY WS-NUMBER
+                   DISPLAY WS-START
                END-IF
 
            END-PERFORM.
